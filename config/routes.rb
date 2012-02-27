3d538b94fe561cc3735c1599cd565831da1503ee
Sean Grove <s@bushi.do>
2012-02-26 19:53:53 -0800

    Updates routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Newsful::Application.routes.draw do
-  resources :votes
+  devise_for :users, :admins
+
   resources :posts do 
     resources :comments
+    resources :votes
   end
-
-  devise_for :users, :admins
 
   resources :users do
     resources :comments
+    resources :votes
   end
 
-  resources :comments
+  resources :comments do
+    resources :votes
+  end
 
   root :to => 'posts#index'
 end
