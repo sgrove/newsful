9c440de67a628d5c9d37ca1e3bcbcee2230b6785
Sacha Greif <sacha357@gmail.com>
2012-03-03 18:18:55 +0100

    made first and last name optional

--- lib/newsful/user_behavior.rb
+++ lib/newsful/user_behavior.rb
@@ -6,9 +6,6 @@ module Newsful
         has_many :posts, :as => :owner
         has_many :votes, :as => :voter
 
-        validates_presence_of :first_name
-        validates_presence_of :last_name
-
         before_validation Proc.new { |user| user.points ||= 1 }
       end
     end
