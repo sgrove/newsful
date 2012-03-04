adc293b3ba24d047a03f49ff4616be1d2d6c7346
Sacha Greif <sacha357@gmail.com>
2012-03-04 11:50:31 +0100

    split nab bar in two

--- db/schema.rb
+++ db/schema.rb
@@ -15,17 +15,12 @@ ActiveRecord::Schema.define(:version => 50) do
 
   create_table "admins", :force => true do |t|
     t.string   "ido_id"
-    t.string   "email"
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",      :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "username"
+    t.string   "email"
     t.string   "first_name"
     t.string   "last_name"
     t.text     "about"
@@ -62,7 +57,6 @@ ActiveRecord::Schema.define(:version => 50) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "ido_id"
     t.string   "email"
     t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
@@ -73,7 +67,7 @@ ActiveRecord::Schema.define(:version => 50) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "username"
+    t.string   "ido_id"
     t.string   "first_name"
     t.string   "last_name"
     t.text     "about"
